5200c22be21165773e043d8484c44121ac8306c4
ACharLuk <acharluk@gmail.com>
2017-02-12 20:35:12 +0100

    Player movement

--- player.js
+++ player.js
@@ -1,8 +1,16 @@
 function Player() {
     this.pos = createVector(0, 0);
+    this.vel = createVector(0, 0);
     this.radius = 10;
 
+    this.update = function() {
+        var acc = createVector(mouseX - width / 2, mouseY - height / 2);
+        acc.setMag(3);
+        this.vel.lerp(acc, 0.2);
+        this.pos.add(this.vel);
+    }
+
     this.show = function() {
-        ellipse(0, 0, this.radius * 2, this.radius * 2);
+        ellipse(this.pos.x, this.pos.y, this.radius * 2, this.radius * 2);
     }
 }
--- sketch.js
+++ sketch.js
@@ -13,10 +13,12 @@ function setup() {
 
 function draw() {
     background(0);
-    translate(width / 2, height / 2);
+    translate(width / 2 - player.pos.x, height / 2 - player.pos.y);
+
+    player.update();
 
     for (i = 0; i < 10; i++)
         blobs[i].show();
-        
+
     player.show();
 }
