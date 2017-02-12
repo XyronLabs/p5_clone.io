a64815584e57b3e381cd5150c8eddd0c4f3c706b
ACharLuk <acharluk@gmail.com>
2017-02-12 20:18:41 +0100

    Blobs!

---
+++ blob.js
@@ -0,0 +1,8 @@
+function Blob() {
+    this.pos = createVector(random(width), random(height));
+    this.radius = 5;
+
+    this.show = function() {
+        ellipse(this.pos.x, this.pos.y, this.radius * 2, this.radius * 2);
+    }
+}
--- sketch.js
+++ sketch.js
@@ -1,14 +1,22 @@
 var player;
 
+var blobs = [];
+
 function setup() {
     createCanvas(800, 480);
 
     player = new Player();
+
+    for (i = 0; i < 10; i++)
+        blobs.push(new Blob());
 }
 
 function draw() {
     background(0);
     translate(width / 2, height / 2);
 
+    for (i = 0; i < 10; i++)
+        blobs[i].show();
+        
     player.show();
 }
