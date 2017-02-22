17dcdfda607a55cc8a201f41a272d7d9f3547c07
ACharLuk <acharluk@gmail.com>
2017-02-22 21:11:38 +0100

    Prototypes!

--- blob.js
+++ blob.js
@@ -4,48 +4,54 @@ function Blob(x, y, r) {
     this.dir = createVector(random(0, width), random(0, height));
     this.radius = r;
     this.color = random(255);
+}
 
-    this.show = function() {
-        ellipse(this.pos.x, this.pos.y, this.radius * 2, this.radius * 2);
-    }
+// Only used in player
+Blob.prototype.update = function() {
+    this.move(mouseX, mouseY);
+}
 
-    // Only used in player
-    this.update = function() {
-        this.move(mouseX, mouseY);
-    }
+Blob.prototype.moveRandom = function() {
+    if (random() > 0.97)
+        this.dir = createVector(random(0, width), random(0, height));
+    this.move(this.dir.x, this.dir.y);
+}
 
-    this.moveRandom = function() {
-        if (random() > 0.97)
-            this.dir = createVector(random(0, width), random(0, height));
-        this.move(this.dir.x, this.dir.y);
-    }
+Blob.prototype.move = function(x, y) {
+    var acc = createVector(x - width / 2, y - height / 2);
+    acc.setMag(3);
+    this.vel.lerp(acc, 0.2);
+    if (this.pos.x + this.vel.x > -width * mapSize && this.pos.x + this.vel.x < width * mapSize)
+        this.pos.x += this.vel.x;
+    if (this.pos.y + this.vel.y > -height * mapSize && this.pos.y + this.vel.y < height * mapSize)
+        this.pos.y += this.vel.y;
+}
 
-    this.move = function(x, y) {
-        var acc = createVector(x - width / 2, y - height / 2);
-        acc.setMag(3);
-        this.vel.lerp(acc, 0.2);
-        if (this.pos.x + this.vel.x > -width * mapSize && this.pos.x + this.vel.x < width * mapSize)
-            this.pos.x += this.vel.x;
-        if (this.pos.y + this.vel.y > -height * mapSize && this.pos.y + this.vel.y < height * mapSize)
-            this.pos.y += this.vel.y;
-    }
+Blob.prototype.move = function(x, y) {
+    var acc = createVector(x - width / 2, y - height / 2);
+    acc.setMag(3);
+    this.vel.lerp(acc, 0.2);
+    if (this.pos.x + this.vel.x > -width * mapSize && this.pos.x + this.vel.x < width * mapSize)
+        this.pos.x += this.vel.x;
+    if (this.pos.y + this.vel.y > -height * mapSize && this.pos.y + this.vel.y < height * mapSize)
+        this.pos.y += this.vel.y;
+}
 
-    this.collided = function(p) {
-        return p != null && p5.Vector.dist(this.pos, p.pos) < this.radius + p.radius;
-    }
+Blob.prototype.collided = function(p) {
+    return p != null && p5.Vector.dist(this.pos, p.pos) < this.radius + p.radius;
+}
 
-    this.show = function() {
-        fill(this.color, 255, 255);
-        ellipse(this.pos.x, this.pos.y, this.radius * 2, this.radius * 2);
+Blob.prototype.show = function() {
+    fill(this.color, 255, 255);
+    ellipse(this.pos.x, this.pos.y, this.radius * 2, this.radius * 2);
 
-        noFill();
-        stroke(this.color, 255, 90);
-        strokeWeight(this.radius / 15);
-        ellipse(this.pos.x, this.pos.y, this.radius * 2, this.radius * 2);
-        
-        fill(this.color, 255, 40);
-        textSize(this.radius * 0.5);
-        textAlign(CENTER, CENTER);
-        text(floor(this.radius), this.pos.x, this.pos.y);
-    }
+    noFill();
+    stroke(this.color, 255, 90);
+    strokeWeight(this.radius / 15);
+    ellipse(this.pos.x, this.pos.y, this.radius * 2, this.radius * 2);
+    
+    fill(this.color, 255, 40);
+    textSize(this.radius * 0.5);
+    textAlign(CENTER, CENTER);
+    text(floor(this.radius), this.pos.x, this.pos.y);
 }
