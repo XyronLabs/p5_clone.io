480773a1b616f83e07fe3f0aae623420c2841e3b
ACharLuk <acharluk@gmail.com>
2017-10-07 08:59:34 +0200

    Methods inside class

--- blob.js
+++ blob.js
@@ -6,49 +6,39 @@ class Blob {
         this.radius = r;
         this.color = random(255);
     }
-}
 
-Blob.prototype.moveRandom = function() {
-    if (random() > 0.97)
-        this.dir = createVector(random(0, width), random(0, height));
-    this.move(this.dir.x, this.dir.y);
-}
+    moveRandom() {
+        if (random() > 0.97)
+            this.dir = createVector(random(0, width), random(0, height));
+        this.move(this.dir.x, this.dir.y);
+    }
 
-Blob.prototype.move = function(x, y) {
-    var acc = createVector(x - width / 2, y - height / 2);
-    acc.setMag(3);
-    this.vel.lerp(acc, 0.2);
-    if (this.pos.x + this.vel.x > -width * mapSize && this.pos.x + this.vel.x < width * mapSize)
-        this.pos.x += this.vel.x;
-    if (this.pos.y + this.vel.y > -height * mapSize && this.pos.y + this.vel.y < height * mapSize)
-        this.pos.y += this.vel.y;
-}
+    move(x, y) {
+        var acc = createVector(x - width / 2, y - height / 2);
+        acc.setMag(3);
+        this.vel.lerp(acc, 0.2);
+        if (this.pos.x + this.vel.x > -width * mapSize && this.pos.x + this.vel.x < width * mapSize)
+            this.pos.x += this.vel.x;
+        if (this.pos.y + this.vel.y > -height * mapSize && this.pos.y + this.vel.y < height * mapSize)
+            this.pos.y += this.vel.y;
+    }
 
-Blob.prototype.move = function(x, y) {
-    var acc = createVector(x - width / 2, y - height / 2);
-    acc.setMag(3);
-    this.vel.lerp(acc, 0.2);
-    if (this.pos.x + this.vel.x > -width * mapSize && this.pos.x + this.vel.x < width * mapSize)
-        this.pos.x += this.vel.x;
-    if (this.pos.y + this.vel.y > -height * mapSize && this.pos.y + this.vel.y < height * mapSize)
-        this.pos.y += this.vel.y;
-}
+    collided(p) {
+        return p != null && p5.Vector.dist(this.pos, p.pos) < this.radius + p.radius;
+    }
 
-Blob.prototype.collided = function(p) {
-    return p != null && p5.Vector.dist(this.pos, p.pos) < this.radius + p.radius;
-}
-
-Blob.prototype.show = function() {
-    fill(this.color, 255, 255);
-    ellipse(this.pos.x, this.pos.y, this.radius * 2, this.radius * 2);
-
-    noFill();
-    stroke(this.color, 255, 90);
-    strokeWeight(this.radius / 15);
-    ellipse(this.pos.x, this.pos.y, this.radius * 2, this.radius * 2);
+    show() {
+        fill(this.color, 255, 255);
+        ellipse(this.pos.x, this.pos.y, this.radius * 2, this.radius * 2);
     
-    fill(this.color, 255, 40);
-    textSize(this.radius * 0.5);
-    textAlign(CENTER, CENTER);
-    text(floor(this.radius), this.pos.x, this.pos.y);
+        noFill();
+        stroke(this.color, 255, 90);
+        strokeWeight(this.radius / 15);
+        ellipse(this.pos.x, this.pos.y, this.radius * 2, this.radius * 2);
+        
+        fill(this.color, 255, 40);
+        textSize(this.radius * 0.5);
+        textAlign(CENTER, CENTER);
+        text(floor(this.radius), this.pos.x, this.pos.y);
+    }
 }
--- player.js
+++ player.js
@@ -2,8 +2,7 @@ class Player extends Blob {
     constructor(x, y, r){
         super(x, y, r);
     }
-}
-// Only used in player
-Player.prototype.update = function() {
-    this.move(mouseX, mouseY);
+    update() {
+        this.move(mouseX, mouseY);
+    }
 }
